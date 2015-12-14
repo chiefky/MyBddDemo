abe22e4f34596b578f09f1006724e43f93f086e1
kangying <kangying@zuche.com>
2015-12-14 17:45:56 +0800

    first demo create

---
+++ MyBddTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBddTestDemo
+//
+//  Created by ciefky on 15/12/14.
+//  Copyright © 2015年 ciefky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBddTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyBddTestDemo
+//
+//  Created by ciefky on 15/12/14.
+//  Copyright © 2015年 ciefky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
